e7629f3583ff4c1dbc8c2434ff4368c7bd2f0a1d
sakellaro <sakella.pan@gmail.com>
2021-01-09 02:05:22 +0200

    Laryngoscope with Spotlight

---
+++ Assets/Scripts/LightON.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightON : MonoBehaviour
+{
+    public RotateON RotateON_script;
+    private Light myLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (RotateON_script.lightON) myLight.enabled = true;
+        else myLight.enabled = false;
+    }
+}
---
+++ Assets/Scripts/RotateON.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateON : MonoBehaviour
+{
+    float speed = 50.0f;
+    public bool lightON;
+    bool rotateUp;
+    bool rotateDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1") && transform.rotation.eulerAngles.x <= 272) rotateDown = true;
+        if (Input.GetKeyDown("2") && transform.rotation.eulerAngles.x >= 357) rotateUp = true;
+        
+        if (rotateUp)
+        {
+            transform.Rotate(Vector3.left * speed * Time.deltaTime);
+            if (transform.rotation.eulerAngles.x <= 272)
+            {
+                GetComponent<AudioSource>().Play();
+                lightON = true;
+                rotateUp = false;
+            }
+        }
+
+        if (rotateDown)
+        {
+            transform.Rotate(Vector3.right * speed * Time.deltaTime);
+            if (transform.rotation.eulerAngles.x >= 357) rotateDown = false;
+        }
+
+        if (transform.rotation.eulerAngles.x > 272)
+        {
+            lightON = false;
+        }
+    }
+}
